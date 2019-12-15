013859f195b95971e8405ab530bc15e8be2fec84
TheAlterBlack <alterblackoffical@gmail.com>
2019-12-16 00:43:05 +0200

    Base for admin pannel

---
+++ models/Article.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property int $id
+ * @property string|null $title
+ * @property string|null $description
+ * @property string|null $content
+ * @property string|null $date
+ * @property string|null $image
+ * @property int|null $viewed
+ * @property int|null $user_id
+ * @property int|null $status
+ * @property int|null $category_id
+ *
+ * @property ArticleTag[] $articleTags
+ * @property Comment[] $comments
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['description', 'content'], 'string'],
+            [['date'], 'safe'],
+            [['viewed', 'user_id', 'status', 'category_id'], 'integer'],
+            [['title', 'image'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'description' => 'Description',
+            'content' => 'Content',
+            'date' => 'Date',
+            'image' => 'Image',
+            'viewed' => 'Viewed',
+            'user_id' => 'User ID',
+            'status' => 'Status',
+            'category_id' => 'Category ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getArticleTags()
+    {
+        return $this->hasMany(ArticleTag::className(), ['article_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getComments()
+    {
+        return $this->hasMany(Comment::className(), ['article_id' => 'id']);
+    }
+}
