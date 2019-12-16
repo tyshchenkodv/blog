32791161563ed007add6e47c9023b67ee13c953b
TheAlterBlack <alterblackoffical@gmail.com>
2019-12-16 07:38:20 +0200

    Added view for article

--- views/site/single.php
+++ views/site/single.php
@@ -1,87 +1,3 @@
-﻿ <!DOCTYPE html>
-<html lang="en">
-
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="Rubel Miah">
-
-    <!-- favicon icon -->
-    <link rel="shortcut icon" href="assets/images/index.html">
-
-    <title>Treasure</title>
-
-    <!-- common css -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/animate.min.css">
-    <link rel="stylesheet" href="assets/css/owl.carousel.css">
-    <link rel="stylesheet" href="assets/css/owl.theme.css">
-    <link rel="stylesheet" href="assets/css/owl.transitions.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="assets/css/responsive.css">
-
-    <!-- HTML5 shim and Respond.js IE9 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="assets/js/html5shiv.js"></script>
-    <script src="assets/js/respond.js"></script>
-    <![endif]-->
-
-    <!-- Favicon -->
-    <link rel="icon" type="image/png" href="assets/images/favicon.png">
-</head>
-<body>
-
-<nav class="navbar main-menu navbar-default">
-    <div class="container">
-        <div class="menu-content">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.html"><img src="assets/images/logo.jpg" alt=""></a>
-            </div>
-
-
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-                <ul class="nav navbar-nav text-uppercase">
-                    <li><a data-toggle="dropdown" class="dropdown-toggle" href="index.html">Home</a>
-                        
-                    </li>
-                </ul>
-                <div class="i_con">
-                    <ul class="nav navbar-nav text-uppercase">
-                        <li><a href="#">Login</a></li>
-                        <li><a href="#">Register</a></li>
-                    </ul>
-                </div>
-
-            </div>
-            <!-- /.navbar-collapse -->
-
-
-            <div class="show-search">
-                <form role="search" method="get" id="searchform" action="#">
-                    <div>
-                        <input type="text" placeholder="Search and hit enter..." name="s" id="s">
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    <!-- /.container-fluid -->
-</nav>
-
-
 <!--main content start-->
 <div class="main-content">
     <div class="container">
@@ -89,7 +5,7 @@
             <div class="col-md-8">
                 <article class="post">
                     <div class="post-thumb">
-                        <a href="blog.html"><img src="assets/images/blog-1.jpg" alt=""></a>
+                        <a href="blog.html"><img src="/public/images/blog-1.jpg" alt=""></a>
                     </div>
                     <div class="post-content">
                         <header class="entry-header text-center text-uppercase">
@@ -170,7 +86,7 @@
 
                         <div class="social-share">
 							<span
-                                    class="social-share-title pull-left text-capitalize">By Rubel On February 12, 2016</span>
+                                class="social-share-title pull-left text-capitalize">By Rubel On February 12, 2016</span>
                             <ul class="text-center pull-right">
                                 <li><a class="s-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
                                 <li><a class="s-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
@@ -182,7 +98,7 @@
                     </div>
                 </article>
                 <div class="top-comment"><!--top comment-->
-                    <img src="assets/images/comment.jpg" class="pull-left img-circle" alt="">
+                    <img src="/public/images/comment.jpg" class="pull-left img-circle" alt="">
                     <h4>Rubel Miah</h4>
 
                     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy hello ro mod tempor
@@ -192,7 +108,7 @@
                     <div class="col-md-6">
                         <div class="single-blog-box">
                             <a href="#">
-                                <img src="assets/images/blog-next.jpg" alt="">
+                                <img src="/public/images/blog-next.jpg" alt="">
 
                                 <div class="overlay">
 
@@ -209,7 +125,7 @@
                     <div class="col-md-6">
                         <div class="single-blog-box">
                             <a href="#">
-                                <img src="assets/images/blog-next.jpg" alt="">
+                                <img src="/public/images/blog-next.jpg" alt="">
 
                                 <div class="overlay">
                                     <div class="promo-text">
@@ -229,7 +145,7 @@
                     <div class="items">
                         <div class="single-item">
                             <a href="#">
-                                <img src="assets/images/related-post-1.jpg" alt="">
+                                <img src="/public/images/related-post-1.jpg" alt="">
 
                                 <p>Just Wondering at Beach</p>
                             </a>
@@ -238,7 +154,7 @@
 
                         <div class="single-item">
                             <a href="#">
-                                <img src="assets/images/related-post-2.jpg" alt="">
+                                <img src="/public/images/related-post-2.jpg" alt="">
 
                                 <p>Just Wondering at Beach</p>
                             </a>
@@ -247,7 +163,7 @@
 
                         <div class="single-item">
                             <a href="#">
-                                <img src="assets/images/related-post-3.jpg" alt="">
+                                <img src="/public/images/related-post-3.jpg" alt="">
 
                                 <p>Just Wondering at Beach</p>
                             </a>
@@ -256,7 +172,7 @@
 
                         <div class="single-item">
                             <a href="#">
-                                <img src="assets/images/related-post-1.jpg" alt="">
+                                <img src="/public/images/related-post-1.jpg" alt="">
 
                                 <p>Just Wondering at Beach</p>
                             </a>
@@ -264,7 +180,7 @@
 
                         <div class="single-item">
                             <a href="#">
-                                <img src="assets/images/related-post-2.jpg" alt="">
+                                <img src="/public/images/related-post-2.jpg" alt="">
 
                                 <p>Just Wondering at Beach</p>
                             </a>
@@ -273,7 +189,7 @@
 
                         <div class="single-item">
                             <a href="#">
-                                <img src="assets/images/related-post-3.jpg" alt="">
+                                <img src="/public/images/related-post-3.jpg" alt="">
 
                                 <p>Just Wondering at Beach</p>
                             </a>
@@ -284,7 +200,7 @@
                     <h4>3 comments</h4>
 
                     <div class="comment-img">
-                        <img class="img-circle" src="assets/images/comment-img.jpg" alt="">
+                        <img class="img-circle" src="/public/images/comment-img.jpg" alt="">
                     </div>
 
                     <div class="comment-text">
@@ -344,7 +260,7 @@
                         <div class="popular-post">
 
 
-                            <a href="#" class="popular-img"><img src="assets/images/p1.jpg" alt="">
+                            <a href="#" class="popular-img"><img src="/public/images/p1.jpg" alt="">
 
                                 <div class="p-overlay"></div>
                             </a>
@@ -357,7 +273,7 @@
                         </div>
                         <div class="popular-post">
 
-                            <a href="#" class="popular-img"><img src="assets/images/p1.jpg" alt="">
+                            <a href="#" class="popular-img"><img src="/public/images/p1.jpg" alt="">
 
                                 <div class="p-overlay"></div>
                             </a>
@@ -370,7 +286,7 @@
                         <div class="popular-post">
 
 
-                            <a href="#" class="popular-img"><img src="assets/images/p1.jpg" alt="">
+                            <a href="#" class="popular-img"><img src="/public/images/p1.jpg" alt="">
 
                                 <div class="p-overlay"></div>
                             </a>
@@ -388,7 +304,7 @@
 
                             <div class="media">
                                 <div class="media-left">
-                                    <a href="#" class="popular-img"><img src="assets/images/r-p.jpg" alt="">
+                                    <a href="#" class="popular-img"><img src="/public/images/r-p.jpg" alt="">
 
                                         <div class="p-overlay"></div>
                                     </a>
@@ -404,7 +320,7 @@
 
                             <div class="media">
                                 <div class="media-left">
-                                    <a href="#" class="popular-img"><img src="assets/images/r-p.jpg" alt="">
+                                    <a href="#" class="popular-img"><img src="/public/images/r-p.jpg" alt="">
 
                                         <div class="p-overlay"></div>
                                     </a>
@@ -420,7 +336,7 @@
 
                             <div class="media">
                                 <div class="media-left">
-                                    <a href="#" class="popular-img"><img src="assets/images/r-p.jpg" alt="">
+                                    <a href="#" class="popular-img"><img src="/public/images/r-p.jpg" alt="">
 
                                         <div class="p-overlay"></div>
                                     </a>
@@ -436,7 +352,7 @@
 
                             <div class="media">
                                 <div class="media-left">
-                                    <a href="#" class="popular-img"><img src="assets/images/r-p.jpg" alt="">
+                                    <a href="#" class="popular-img"><img src="/public/images/r-p.jpg" alt="">
 
                                         <div class="p-overlay"></div>
                                     </a>
@@ -483,176 +399,3 @@
     </div>
 </div>
 <!-- end main content-->
-<!--footer start-->
-<div id="footer">
-    <div class="footer-instagram-section">
-        <h3 class="footer-instagram-title text-center text-uppercase">Instagram</h3>
-
-        <div id="footer-instagram" class="owl-carousel">
-
-            <div class="item">
-                <a href="#"><img src="assets/images/ins-1.jpg" alt=""></a>
-            </div>
-            <div class="item">
-                <a href="#"><img src="assets/images/ins-2.jpg" alt=""></a>
-            </div>
-            <div class="item">
-                <a href="#"><img src="assets/images/ins-3.jpg" alt=""></a>
-            </div>
-            <div class="item">
-                <a href="#"><img src="assets/images/ins-4.jpg" alt=""></a>
-            </div>
-            <div class="item">
-                <a href="#"><img src="assets/images/ins-5.jpg" alt=""></a>
-            </div>
-            <div class="item">
-                <a href="#"><img src="assets/images/ins-6.jpg" alt=""></a>
-            </div>
-            <div class="item">
-                <a href="#"><img src="assets/images/ins-7.jpg" alt=""></a>
-            </div>
-            <div class="item">
-                <a href="#"><img src="assets/images/ins-8.jpg" alt=""></a>
-            </div>
-
-        </div>
-    </div>
-</div>
-
-<footer class="footer-widget-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <aside class="footer-widget">
-                    <div class="about-img"><img src="assets/images/logo2.png" alt="Kotha"></div>
-                    <div class="about-content">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                        diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-                        voluptua. At vero eos et accusam et justo duo dlores et ea rebum magna text ar koto din.
-                    </div>
-                    <div class="address">
-                        <h4 class="text-uppercase">contact Info</h4>
-
-                        <p> 239/2 NK Street, DC, USA</p>
-
-                        <p> Phone: +123 456 78900</p>
-
-                    </div>
-                </aside>
-            </div>
-
-            <div class="col-md-4">
-                <aside class="footer-widget">
-                    <h3 class="widget-title text-uppercase">Testimonials</h3>
-
-                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                        <!--Indicator-->
-                        <ol class="carousel-indicators">
-                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                            <li data-target="#myCarousel" data-slide-to="1"></li>
-                            <li data-target="#myCarousel" data-slide-to="2"></li>
-                        </ol>
-                        <div class="carousel-inner" role="listbox">
-                            <div class="item active">
-                                <div class="single-review">
-                                    <div class="review-text">
-                                        <p>Lorem ipsum dolor sit amet, conssadipscing elitr, sed diam nonumy eirmod
-                                            tempvidunt ut labore et dolore magna aliquyam erat,sed diam voluptua. At
-                                            vero eos et accusam justo duo dolores et ea rebum.gubergren no sea takimata
-                                            magna aliquyam eratma</p>
-                                    </div>
-                                    <div class="author-id">
-                                        <img src="assets/images/author.png" alt="">
-
-                                        <div class="author-text">
-                                            <h4>Anthony DiPrizio</h4>
-
-                                            <h4>CEO</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="item">
-                                <div class="single-review">
-                                    <div class="review-text">
-                                        <p>Lorem ipsum dolor sit amet, conssadipscing elitr, sed diam nonumy eirmod
-                                            tempvidunt ut labore et dolore magna aliquyam erat,sed diam voluptua. At
-                                            vero eos et accusam justo duo dolores et ea rebum.gubergren no sea takimata
-                                            magna aliquyam eratma</p>
-                                    </div>
-                                    <div class="author-id">
-                                        <img src="assets/images/author.png" alt="">
-
-                                        <div class="author-text">
-                                            <h4>Anthony DiPrizio</h4>
-
-                                            <h4>CEO</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="item">
-                                <div class="single-review">
-                                    <div class="review-text">
-                                        <p>Lorem ipsum dolor sit amet, conssadipscing elitr, sed diam nonumy eirmod
-                                            tempvidunt ut labore et dolore magna aliquyam erat,sed diam voluptua. At
-                                            vero eos et accusam justo duo dolores et ea rebum.gubergren no sea takimata
-                                            magna aliquyam eratma</p>
-                                    </div>
-                                    <div class="author-id">
-                                        <img src="assets/images/author.png" alt="">
-
-                                        <div class="author-text">
-                                            <h4>Anthony DiPrizio</h4>
-
-                                            <h4>CEO</h4>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </aside>
-            </div>
-            <div class="col-md-4">
-                <aside class="footer-widget">
-                    <h3 class="widget-title text-uppercase">Custom Category Post</h3>
-
-
-                    <div class="custom-post">
-                        <div>
-                            <a href="#"><img src="assets/images/footer-img.png" alt=""></a>
-                        </div>
-                        <div>
-                            <a href="#" class="text-uppercase">Home is peaceful Place</a>
-                            <span class="p-date">February 15, 2016</span>
-                        </div>
-                    </div>
-                </aside>
-            </div>
-        </div>
-    </div>
-    <div class="footer-copy">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="text-center">&copy; 2015 <a href="#">Treasure PRO, </a> Built with <i
-                            class="fa fa-heart"></i> by <a href="#">Rahim</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
-
-<!-- js files -->
-<script type="text/javascript" src="assets/js/jquery-1.11.3.min.js"></script>
-<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
-<script type="text/javascript" src="assets/js/jquery.stickit.min.js"></script>
-<script type="text/javascript" src="assets/js/menu.js"></script>
-<script type="text/javascript" src="assets/js/scripts.js"></script>
-</body>
-
-
-</html>
