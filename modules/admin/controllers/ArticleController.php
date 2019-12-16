407f630d6aa8c9d8e76d0dca82d668b24aa052d3
TheAlterBlack <alterblackoffical@gmail.com>
2019-12-16 02:12:33 +0200

    Add pictures
    Add view for adding pictures

--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace app\modules\admin\controllers;
 
+use app\models\ImageUpload;
 use Yii;
 use app\models\Article;
 use app\models\ArticleSearch;
@@ -123,5 +124,12 @@ class ArticleController extends Controller
         }
 
         throw new NotFoundHttpException('The requested page does not exist.');
+    }
+
+    public function actionSetImage($id)
+    {
+        $model = new Article;
+
+        return $this->render('image', ['model' => $model]);
     }
 }
